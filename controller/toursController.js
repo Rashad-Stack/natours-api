46c04ec015b07b45ca7f70fdfe10c96aef83f3d3
Rashad-Stack <tex.eng.rashed@gmail.com>
2023-02-06 15:33:46 +0600

    agrregiation pipeline filter added

--- controller/toursController.js
+++ controller/toursController.js
@@ -1,11 +1,46 @@
 const Tour = require("../models/tourModels");
 const APIFeatures = require("../utils/apiFeatures");
 
-exports.aliasTopTour = async (req, res, next) => {
+exports.aliasTopTour = (req, res, next) => {
   req.query.limit = 5;
   req.query.sort = "-ratingsAverage,price";
   req.query.fields = "name,price,ratingsAverage,summery,difficulty";
   next();
+};
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: "$difficulty" },
+          totalTours: { $sum: 1 },
+          totalRatings: { $sum: "$ratingQuantity" },
+          avgRating: { $avg: "$ratingsAverage" },
+          avgPrice: { $avg: "$price" },
+          minPrice: { $min: "$price" },
+          maxPrice: { $max: "$price" },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+      // { $match: { _id: { $ne: "EASY" } } },
+    ]);
+    res.status(200).json({
+      status: "success",
+      results: stats.length,
+      data: stats,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "failed",
+      error,
+    });
+  }
 };
 
 exports.getAllTours = async (req, res) => {
