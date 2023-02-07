6739beca68d291f91e95743dcda261624a91de0d
Rashad-Stack <tex.eng.rashed@gmail.com>
2023-02-08 05:24:52 +0600

    Added incripted password

--- models/userModels.js
+++ models/userModels.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -27,7 +28,26 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, "Please confirm your password"],
+    validate: {
+      // This only work on create and save method
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Password are not same!",
+    },
   },
+});
+
+userSchema.pre("save", async function (next) {
+  // Only run this function when password was actually modified;
+  if (!this.isModified("password")) return next();
+
+  //   Hash the password with coast of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //   cleared the password confirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model("Users", userSchema);
