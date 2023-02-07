6762404d096ee244c8e1c732a73a48c389850f1b
Rashad-Stack <tex.eng.rashed@gmail.com>
2023-02-07 17:43:29 +0600

    Preparing for custom Error handling

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const app = express();
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
+app.use((req, res, next) => {
+  req.requestedTime = new Date().toISOString();
+  next();
+});
+
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -16,5 +21,27 @@ if (process.env.NODE_ENV === "development") {
 // routes middleware
 app.use("/api/v1/tours", toursRoutes);
 app.use("/api/v1/users", userRoutes);
+
+app.all("*", (req, res, next) => {
+  // res.status(404).json({
+  //   status: "Failed",
+  //   message: `Can't Find this URL (${req.originalUrl}) on this server!`,
+  // });
+  // next();
+
+  const err = new Error(
+    `Can't Find this URL (${req.originalUrl}) on this server!`
+  );
+  err.statusCode = 404;
+  err.status = "Failed";
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500).json({
+    status: err.status || "Error",
+    message: err.message,
+  });
+});
 
 module.exports = app;
