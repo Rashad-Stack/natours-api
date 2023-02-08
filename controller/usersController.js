6229fcedb858d0e4af25d7c0ff3b2a080680da57
Rashad-Stack <tex.eng.rashed@gmail.com>
2023-02-08 23:24:54 +0600

    Login and signup with jwt token

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,9 +1,15 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: "fail",
-    message: "This route does not define yet",
+const User = require("../models/userModels");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getAllUsers = catchAsync(async (req, res) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: "success",
+    results: users.length,
+    data: users,
   });
-};
+});
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: "fail",
