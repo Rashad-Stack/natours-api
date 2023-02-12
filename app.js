867ac2738c7701cf8c4afe78c69458a26dbcc79f
Rashad-Stack <tex.eng.rashed@gmail.com>
2023-02-12 23:42:39 +0600

    added helmet and limit packages

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 const toursRoutes = require("./routes/tourRoutes");
 const userRoutes = require("./routes/userRoutes");
 const AppError = require("./utils/appError");
@@ -8,23 +9,29 @@ const globalErrorHandler = require("./controller/errorControllers");
 
 // Global Middle wares
 const app = express();
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
-
-app.use((req, res, next) => {
-  req.requestedTime = new Date().toISOString();
-  next();
-});
+// Set secure HTTP headers
+app.use(helmet());
 
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Limit request from same api
 const limit = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: "Too many request from this IP. please try again in an hour!",
+});
+
+//  Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+app.use(express.static(`${__dirname}/public`));
+
+//  Test Middleware
+app.use((req, res, next) => {
+  req.requestedTime = new Date().toISOString();
+  next();
 });
 
 // routes middleware
